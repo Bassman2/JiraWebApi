eaae27b598df3141412276e1c78709785a29a78e
Ralf Beckers <ralf.beckers@hotmail.com>
2019-02-15 12:27:09 +0100

    Update Newtonsoft.Json

--- JiraWebApi/Properties/AssemblyInfo.cs
+++ JiraWebApi/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Ralf Beckers")]
 [assembly: AssemblyProduct("JiraWebApi")]
-[assembly: AssemblyCopyright("Copyright © Ralf Beckers 2013 - 2017")]
+[assembly: AssemblyCopyright("Copyright © Ralf Beckers 2013 - 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.3.1.0")]
-[assembly: AssemblyFileVersion("1.3.1.0")]
+[assembly: AssemblyVersion("1.3.2.0")]
+[assembly: AssemblyFileVersion("1.3.2.0")]
